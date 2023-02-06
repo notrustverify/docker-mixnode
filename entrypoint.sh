1bf9d6edbe45493e463ed596a1278562d49be314
cgi-bin <s@ono.re>
2023-02-06 20:43:49 +0100

    fix: bugs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,21 @@ set -e
 
 NAME_MIXNODE=${NAME_MIXNODE:-"docker-mixnode"}
 MIXNODES_DIR=~/.nym/mixnodes/${NAME_MIXNODE}
+FORCE_INIT=${FORCE_INIT:-false}
 
+EXEC_VERSION=$(./nym-mixnode -V | sed 's:nym-mixnode::' |xargs)
 
-if [ ! -d ${MIXNODES_DIR} ]; then
+if [ -f "${MIXNODES_DIR}/config/config.toml" ]; then
+	CONFIG_VERSION=$(cat "${MIXNODES_DIR}/config/config.toml" | awk -F "=" '/version/ {print $2}' | xargs)
+fi
+
+if [[ ! -v ANNOUNCE_HOST || $EXEC_VERSION != $CONFIG_VERSION || $FORCE_INIT == true ]];then 
    echo "Init nym client"
-    if [[ -v ANNOUNCE_HOST ]]; then
-   	./nym-mixnode init --id $NAME_MIXNODE  --announce-host $ANNOUNCE_HOST --host 0.0.0.0 --wallet-address $WALLET_ADDRESS
-    else
+   if [[ -v ANNOUNCE_HOST ]]; then
+      	./nym-mixnode init --id $NAME_MIXNODE  --announce-host $ANNOUNCE_HOST --host 0.0.0.0 --wallet-address $WALLET_ADDRESS
+       else
         ./nym-mixnode init --id $NAME_MIXNODE --announce-host $(curl ifconfig.me)  --host 0.0.0.0 --wallet-address $WALLET_ADDRESS
-    fi
+   fi
 fi
 
 echo "Run nym client"
